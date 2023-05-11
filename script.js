0537a6377a40132c9d7ba7438fad0061d0e7b015
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-05-11 21:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,15 @@ function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i =0 ;i<encodingStr.length; i++){
-		if(lookup[encodedStr[i]] != undefined){
-			decodedArr.push[i];
+		if(lookup[encodedStr[i]] === undefined){
+			decodedArr.push[encodedStr[i]];
 		}
-		continue;
+		else {
+			decodedArr.push[lookup[encodedStr[i]];
+		}
 	}
 
-  return ;//return decodedArr
+  return  decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
